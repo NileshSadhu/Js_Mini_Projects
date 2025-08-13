440b12efb821d82344510e70ca45b5b3ef07cb89
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2025-08-13 07:49:37 +0530

    NodeJs ToDo Project

---
+++ 09_NodeJs/todo/script.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const filePath = './tasks.json';
+
+const loadTasks = () => {
+    try {
+        // databuffer is a space in memory (usually RAM) that temporarily stores binary data. 
+        const dataBuffer = fs.readFileSync(filePath);
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (error) {
+        return [];
+    }
+}
+
+const listTasks = () => {
+    const Tasks = loadTasks();
+    Tasks.forEach((task, index) => { console.log(`${index + 1}-${task.task}`) });
+}
+
+const saveTasks = (tasks) => {
+    const dataJSON = JSON.stringify(tasks);
+    fs.writeFileSync(filePath, dataJSON);
+}
+
+const addTask = (task) => {
+    const tasks = loadTasks();
+    tasks.push({ task });
+    saveTasks(tasks);
+    console.log('Task added : ', task);
+}
+
+
+// node filename command argument
+// if you want to get command line argument use process.agrv which will 
+// return an array which first 2 spot is taken by node[0] and  filename[1]
+const command = process.argv[2];
+const argument = process.argv[3];
+
+if (command === 'add') {
+    addTask(argument);
+} else if (command === 'list') {
+    listTasks();
+} else if (command === 'remove') {
+    removeTask(parseInt(argument));
+} else {
+    console.log('command not found !');
+}
