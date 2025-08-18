c88fa5ce99411a47fd349bbd5ccfbb5a72b572ef
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2025-08-18 18:05:28 +0530

    Completed Digital Clock Project

--- 10_digital_clock/script.js
+++ 10_digital_clock/script.js
@@ -1,22 +1,26 @@
+setInterval(() => {
 
-let date = new Date();
-const day = date.getDay();
+    let date = new Date();
+    const day = date.getDay();
 
-const Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
-console.log(Days[day]);
+    const Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
+    console.log(Days[day]);
 
-// document.getElementById("showDay").innerText = Days[day];
+    document.getElementById("showDay").innerText = Days[day];
 
-let hour = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
 
-if (hour > 12) {
-    period = "PM";
-} else {
-    period = "AM";
-}
+    let period = hour >= 12 ? "PM" : "AM";
 
+    if (hour === 0) {
+        hour = 12;
+    } else if (hour > 12) {
+        hour -= 12;
+    }
 
-var time = `${hour} : ${minutes} : ${seconds} ${period}`;
-console.log(time); 
+    var time = `${hour} : ${minutes} : ${seconds} ${period}`;
+    document.getElementById("showClock").innerText = time;
+
+}, 1000);
